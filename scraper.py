0cbaa6f40b534e1454ceab57ab6cde8cd67edb56
Marcos Nakagawa <nagawaka@iMac-de-Marcos.local>
2019-09-17 04:03:46 -0300

    added item crawling

--- scraper.py
+++ scraper.py
@@ -1,6 +1,13 @@
 # scrapy runspider scraper.py -o file.json
 
+import logging
+
 import scrapy
+
+class Line(scrapy.Item):
+  title = scrapy.Field()
+  link = scrapy.Field()
+  pdf = scrapy.Field()
 
 class QuotesSpider(scrapy.Spider):
   name = 'quotes'
@@ -8,13 +15,37 @@ class QuotesSpider(scrapy.Spider):
     'https://datassette.org/revistas/acao-games',
   ]
 
+  custom_settings = {
+    'ITEM_PIPELINES': {'scrapy.pipelines.files.FilesPipeline': 1},
+    'FILES_STORE': 'downloads'
+  }
+
+  BASE_URL = 'https://datassette.org'
+
   def parse(self, response):
     for quote in response.css('.view-content td'):
-      yield {
-        'text': quote.css('.views-field-title a::text').get(),
-        'author': quote.css('.views-field-title a::attr("href")').get(),
-      }
+      link = quote.css('.views-field-title a::attr("href")').extract()[0]
+      absolute_url = self.BASE_URL + link
+      line = Line(
+        title=quote.css('.views-field-title a::text').extract()[0],
+        link=link
+      )
 
-    next_page = response.css('li.pager-next a::attr("href")').getall()
+      pdfRequest = scrapy.Request(absolute_url, callback=self.parseLink, cb_kwargs=line)
+
+
+      yield pdfRequest
+
+    next_page = response.css('li.pager-next a::attr("href")').extract()
     if next_page[1] is not None:
       yield response.follow(next_page[1], self.parse)
+  
+  def parseLink(self, response, title, link):
+
+    yield {
+      'link': response.url,
+      'title': title,
+      'file_urls': [response.css('.file a::attr("href")').extract()[0]]
+    }
+    # return 
+  
